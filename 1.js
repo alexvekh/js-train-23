c0f7cb67092c1fa22783fb05b6ce6a6ba6f617db
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-08-06 18:56:58 -0400

    homework

--- 1.js
+++ 1.js
@@ -3,6 +3,16 @@
 
 // Клас ContentContainer використовується для управління списком вкладених елементів контенту
 class ContentContainer {
+  elements = [];
+
+  addElement(element) {
+    this.elements.push(element);
+    //console.log("added", element, this.elements);
+  }
+  removeElement(element) {
+    const index = this.elements.indexOf(element);
+    if (index !== -1) this.elements.splice(index, 1);
+  }
   // Створюємо властивість elements для зберігання вкладених елементів контенту. Початкове значення - порожній масив.
   // Створюємо addElement, який отримує element як параметр та додає його в масив elements.
   // Створюємо removeElement, який отримує element як параметр, знаходить його індекс у масиві та видаляє, якщо елемент знайдено.
@@ -11,30 +21,76 @@ class ContentContainer {
 // Клас Message, що є розширенням класу ContentContainer. Використовується для створення повідомлень з текстом.
 class Message extends ContentContainer {
   // Створюємо конструктор класу, який приймає content як параметр та ініціалізує його
+  constructor(content) {
+    super();
+    this.content = content;
+  }
   // Створюємо метод display, який виводить ${this.content} для всіх елементів масиву
+  display() {
+    console.log(`Повідомлення: ${this.content}`);
+    for (const element of this.elements) element.display();
+  }
+
+  //-from Chat GPT
+  // display(isParent = true) {
+  //   if (isParent) {
+  //     console.log(`Повідомлення: ${this.content}`);
+  //   }
+  //   this.elements.forEach((element) => {
+  //     if (element instanceof Message) {
+  //       console.log(`Повідомлення: ${element.content}`);
+  //     }
+  //     if (element instanceof Message && element.elements.length > 0) {
+  //       element.display(false);
+  //     }
+  //   });
+  // }
 }
 
 // Клас Article, що є розширенням класу ContentContainer. Використовується для створення статті з вкладеними елементами.
 class Article extends ContentContainer {
   // Створюємо конструктор класу, який приймає title назву статті як параметр та ініціалізує об'єкт з нею
+  constructor(title) {
+    super();
+    this.title = title;
+  }
   // Створюємо метод display, який виводить Стаття: ${this.title} для всіх елементів масиву
+  display() {
+    console.log(`Стаття: ${this.title}`);
+    for (const element of this.elements) element.display();
+  }
+
+  //-from Chat GPT
+  // display(isParent = true) {
+  //   if (isParent) {
+  //     console.log(`Стаття: ${this.title}`);
+  //   }
+  //   this.elements.forEach((element) => {
+  //     if (element instanceof Message) {
+  //       console.log(`Повідомлення: ${element.content}`);
+  //     }
+  //     if (element instanceof Message && element.elements.length > 0) {
+  //       element.display(false);
+  //     }
+  //   });
+  // }
 }
 
 console.log("Завдання 1 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створюємо об'єкт Article з назвою "Навчальна стаття"
-// const article = new Article("Навчальна стаття");
+const article = new Article("Навчальна стаття");
 
 // Додаємо повідомлення до статті
-// article.addElement(new Message("Дуже корисна стаття"));
-// article.addElement(new Message("Дякую за чудовий матеріал!"));
+article.addElement(new Message("Дуже корисна стаття"));
+article.addElement(new Message("Дякую за чудовий матеріал!"));
 
 // Додаємо вкладене повідомлення до першого повідомлення в статті
-// article.elements[0].addElement(new Message("Відповідь: Згоден!"));
+article.elements[0].addElement(new Message("Відповідь: Згоден!"));
 
 // Виводимо інформацію про статтю та всі її вкладені елементи
-// article.display();
+article.display();
 
 // Виводимо масив вкладених елементів статті
-// console.log(article.elements);
+console.log(article.elements);
